d28bbf7ba2168f71739411b82adf08cc2d36a160
xin <s0984008299@gmail.com>
2019-10-09 12:59:59 +0800

    logging設定,利用時間管理檔案

--- logging_test.py
+++ logging_test.py
@@ -7,18 +7,19 @@ Created on Thu Oct  3 20:14:47 2019
 
 import logging
 import time
-from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler  #隨著時間去創建一個新檔案
 
 def create_timed_rotating_log(path):
-    logger = logging.getLogger("Rotating Log")
+    logger = logging.getLogger("Rotating Log")  #輸出日誌的總對象
     logger.setLevel(logging.INFO)
     handler = TimedRotatingFileHandler(path,
-                                       when='m',
-                                       interval = 1,
-                                       backupCount = 5)
+                                       when='m',  #每個分鐘
+                                       interval = 1, #1分鐘
+                                       backupCount = 5)  #共有5個文件
     logger.addHandler(handler)
-    for i in range(20):
-        logger.info("This is a test!")
+    #會在timed_test.log顯示20個This is a test!
+    for i in range(20): 
+        logger.info("This is a test!  %s" % (i))
         time.sleep(1.5)
 if __name__ =="__main__":
     log_file = "tomed_test.log"
